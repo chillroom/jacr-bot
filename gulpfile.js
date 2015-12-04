49ca5e1e3b996e3cd9e086776421cadbe2ace3b4
Ben Wildeman <wildeman.benjamin@gmail.com>
2015-12-04 01:40:17 +0000

    why is this being silly

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require("gulp"),
 	rename = require("gulp-rename");
 
 gulp.task("config", function () {
-	return gulp.src("./config.example.js")
-		.pipe(rename("./config.js"))
+	return gulp.src(__dirname + "/config.example.js")
+		.pipe(rename(__dirname + "/config.js"))
 		.pipe(gulp.dest("."));
 });
 
 gulp.task("mine", function () {
-	return gulp.src("./config.mine.js")
-		.pipe(rename("./config.js"))
+	return gulp.src(__dirname + "/config.mine.js")
+		.pipe(rename(__dirname + "/config.js"))
 		.pipe(gulp.dest("."));
 });
 
