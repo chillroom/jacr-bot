5fadd759bc880f5da23a066281906d169e36d9f1
Ben Wildeman <wildeman.benjamin@gmail.com>
2015-12-04 01:47:29 +0000

    removed gulp

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	ipaddress: process.env.OPENSHIFT_NODEJS_IP,
+	port: process.env.OPENSHIFT_NODEJS_PORT,
+	slackUrl: process.env.SLACK_URL,
+	slackChannels: process.env.SLACK_CHANNELS,
+	slackMessage: process.env.SLACK_MESSAGE,
+	slackToken: process.env.SLACK_TOKEN,
+	origins: process.env.SITE_ORIGINS,
+	socialLink: process.env.SOCIAL_LINK,
+	botName: process.env.BOT_NAME,
+	botPass: process.env.BOT_PASS,
+	roomURL: process.env.ROOM_URL
+};
