15eb63c6ea2708eeb19485c8521263da85ca3e4f
Ben Wildeman <wildeman.benjamin@gmail.com>
2015-12-14 21:40:45 +0000

    added !screwyou

---
+++ app/bot/commands/memes/screwyou.js
@@ -0,0 +1,7 @@
+module.exports = function (bot) {
+	var memes = [
+		"http://38.media.tumblr.com/c36b7285b0c15b17c93eba873448cdb6/tumblr_inline_nw32a96HUC1t8es98_500.gif"
+	];
+	var post = memes[Math.floor(Math.random() * memes.length)];
+	bot.sendChat(post);
+};
