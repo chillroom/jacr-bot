81e8712ae8c146def894f0b1f50278a8193161f3
nitroghost <wildeman.benjamin@gmail.com>
2016-01-11 20:08:23 +0000

    added !move command

---
+++ app/bot/commands/mod/move.js
@@ -0,0 +1,30 @@
+module.exports = function (bot, data) {
+	var user = data.user.username;
+	var rank = data.user.role;
+	if (bot.devs.indexOf(user) > -1 || bot.ranks.indexOf(rank) > -1) {
+		if (typeof (data.params) !== "undefined" && data.params.length > 0) {
+			var username = data.params[0];
+			var pos = 3,
+				person;
+			if (data.params.length > 1) {
+				var position = data.params[1];
+				if (username.substr(0, 1) === "@") {
+					//remove the @
+					username = username.substr(1);
+				}
+				if (!isNaN(parseInt(position))) {
+					pos = parseInt(position);
+				}
+				bot.moderateMoveDJ(person.id, pos);
+				bot.sendChat(bot.identifer + username + " moved to position: " + pos);
+			} else {
+				if (username.substr(0, 1) === "@") {
+					//remove the @
+					username = username.substr(1);
+				}
+				bot.moderateMoveDJ(person.id, pos);
+				bot.sendChat(bot.identifer + username + " moved to position: " + pos);
+			}
+		}
+	}
+};
