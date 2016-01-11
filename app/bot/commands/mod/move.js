bebdede49b4e02854b9d5e95f4b8de8ace948455
nitroghost <wildeman.benjamin@gmail.com>
2016-01-11 20:39:32 +0000

    tut

--- app/bot/commands/mod/move.js
+++ app/bot/commands/mod/move.js
@@ -3,9 +3,9 @@ module.exports = function (bot, data) {
 	var rank = data.user.role;
 	if (bot.devs.indexOf(user) > -1 || bot.ranks.indexOf(rank) > -1) {
 		if (typeof (data.params) !== "undefined" && data.params.length > 0) {
-			var username = data.params[0];
-			var pos = 3,
-				person;
+			var username = data.params[0],
+				pos = 3,
+				person = bot.getUserByName(username);
 			if (data.params.length > 1) {
 				var position = data.params[1];
 				if (username.substr(0, 1) === "@") {
