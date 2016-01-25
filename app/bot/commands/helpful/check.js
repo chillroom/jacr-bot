ec0236aa08c0dbf002a152b9e979e9be16f53a86
nitroghost <wildeman.benjamin@gmail.com>
2016-01-25 20:54:26 +0000

    added !check command to search songs

---
+++ app/bot/commands/helpful/check.js
@@ -0,0 +1,28 @@
+module.exports = function(bot, data) {
+    var text = data.params.join(" ");
+    text = text.replace(/-+/g, "").replace(/  +/g, ' ').trim();
+    bot.db.models.song.songs.find({
+        $text: {
+            $search: text
+        }
+    }, {
+        score: {
+            $meta: "textScore"
+        }
+    }).sort({
+        score: {
+            $meta: "textScore"
+        }
+    }).limit(1).exec(function(err, doc) {
+        if (err) {
+            bot.sendChat(bot.identifier + "leaf me alone, I has an error");
+            bot.log("error", "MONGO", err);
+        } else {
+            if (doc) {
+                bot.sendChat(bot.identifier + "song: " + doc.name + " has been played " doc.plays + " before");
+            } else {
+                bot.sendChat(bot.identifier + "way to go sailor, that song hasn't been played before");
+            }
+        }
+    });
+};
