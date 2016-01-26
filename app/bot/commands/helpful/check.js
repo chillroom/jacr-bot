1bad4fbc18ad17492fcf1579a210a7226f94af85
nitroghost <wildeman.benjamin@gmail.com>
2016-01-26 00:15:24 +0000

    replaced find with aggregate for !check

--- app/bot/commands/helpful/check.js
+++ app/bot/commands/helpful/check.js
@@ -1,32 +1,50 @@
 module.exports = function(bot, data) {
     var text = data.params.join(" ");
     text = text.replace(/-+/g, "").replace(/\s+/g, " ").trim();
-    bot.db.models.song.find({
-        $text: {
-            $search: text
-        }
-    }, {
-        score: {
-            $meta: "textScore"
-        }
-    }).sort({
-        score: {
-            $meta: "textScore"
-        }
-    }).limit(1).exec(function(err, doc) {
-        if (err) {
-            bot.sendChat(bot.identifier + "leaf me alone, I has an error");
-            bot.log("error", "MONGO", err);
-        } else {
-            if (doc[0]) {
-                if (doc[0].score > 0.7) {
+    if (typeof(data.params) !== "undefined" && data.params.length > 0) {
+        bot.db.models.song.aggregate([
+            {
+                $match: {
+                    $text: {
+                        $search: text
+                    }
+                }
+            }, {
+                $sort: {
+                    score: {
+                        $meta: "textScore"
+                    }
+                }
+            }, {
+                $project: {
+                    score: {
+                        $meta: "textScore"
+                    },
+                    name: 1,
+                    plays: 1
+                }
+            }, {
+                $match: {
+                    score: {
+                        $gt: 0.7
+                    }
+                }
+            }, {
+                $limit: 1
+            }
+        ]).exec(function(err, doc) {
+            if (err) {
+                bot.sendChat(bot.identifier + "leaf me alone, I has an error");
+                bot.log("error", "MONGO", err);
+            } else {
+                if (doc[0]) {
                     bot.sendChat(bot.identifier + doc[0].name + " has been played " + doc[0].plays + " times before");
                 } else {
-                    bot.sendChat(bot.identifier + "way to go sailor, looks like that song hasn't been played before");
+                    bot.sendChat(bot.identifier + "way to go sailor, that song hasn't been played before");
                 }
-            } else {
-                bot.sendChat(bot.identifier + "way to go sailor, looks like that song hasn't been played before");
             }
-        }
-    });
+        });
+    } else {
+        bot.sendChat(bot.identifier + "aw shucks, you didn't include a search query");
+    }
 };
