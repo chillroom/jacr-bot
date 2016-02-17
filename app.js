1fd679cee0e21172e61ac8e3e3f7650247fc1178
nitroghost <wildeman.benjamin@gmail.com>
2016-02-17 11:47:14 +0000

    oops, forgot this was a thing

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ var log = require("jethro"),
 
 log.setUTC(true);
 
-if (typeof config.slackToken === "undefined") {
+if (typeof config.slack.token === "undefined") {
 	throw Error("Please set the SLACK_TOKEN evironment variable");
-} else if (typeof config.slackUrl === "undefined") {
+} else if (typeof config.slack.URL === "undefined") {
 	throw Error("Please set the SLACK_URL evironment variable");
 } else if (typeof config.origins === "undefined") {
 	throw Error("Please set the SITE_ORIGINS environment variable");
-} else if (typeof config.slackChannels === "undefined") {
+} else if (typeof config.slack.channels === "undefined") {
 	log("warning", "API", "No SLACK_CHANNELS var, ommiting from post");
 	config.slackChannels = "";
-} else if (typeof config.slackMessage === "undefined") {
+} else if (typeof config.slack.message === "undefined") {
 	log("warning", "API", "No SLACK_MESSAGE var, ommiting from post");
 	config.slackMessage = "";
 } else if (typeof config.ipaddress === "undefined") {
 	log("warning", "API", "No OPENSHIFT_NODEJS_IP var, using 127.0.0.1");
 	config.ipaddress = "127.0.0.1";
-} else if (typeof config.botName === "undefined") {
+} else if (typeof config.bot.name === "undefined") {
 	throw Error("Please set the BOT_NAME evironment variable or add bot username to the config.js file");
-} else if (typeof config.botPass === "undefined") {
+} else if (typeof config.bot.pass === "undefined") {
 	throw Error("Please set the BOT_PASS evironment variable or add bot password to the config.js file");
-} else if (typeof config.roomURL === "undefined") {
+} else if (typeof config.bot.URL === "undefined") {
 	throw Error("Please set the ROOM_URL environment variable or add the room URL to the config.js file");
 } else {
 	require("./app/server");
