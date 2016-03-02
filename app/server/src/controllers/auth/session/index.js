a5c754af93d08dc8d33af89bf4734c3e638e89d5
nitroghost <wildeman.benjamin@gmail.com>
2016-03-02 08:14:45 +0000

    added state to session endpoint

--- app/server/src/controllers/auth/session/index.js
+++ app/server/src/controllers/auth/session/index.js
@@ -6,7 +6,7 @@ module.exports = (req, reply) => {
     reply({
       statusCode: 200,
       message: "Session verified."
-  }).header("Authorization", req.headers.authorization).state("token", req.headers.authorization, {
+  }).header("Authorization", req.headers.authorization)state("token", req.state.token, {
       ttl: config.jwt.ttl
   });
 };
