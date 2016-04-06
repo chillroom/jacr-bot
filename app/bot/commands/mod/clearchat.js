792430bcba2562c93e1d8c918ea2217ddf0b1bfc
Qais Patankar <qaisjp@gmail.com>
2016-04-06 13:28:03 +0100

    :fu: mclovinthesex

--- app/bot/commands/mod/clearchat.js
+++ app/bot/commands/mod/clearchat.js
@@ -3,6 +3,6 @@ var request = require("request");
 module.exports = (bot, data) => {
     const user = data.user.username;
     if (user.toLowerCase() === "mclovinthesex") {
-        bot.sendChat(bot.identifier + ":fu: @" + user);
+        bot.sendChat(bot.identifier + ":* @" + user);
     }
 };
