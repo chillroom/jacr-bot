1d65be4e21a0082c35cc31817445b5714aef7743
Qais Patankar <qaisjp@gmail.com>
2016-04-06 13:27:43 +0100

    fix stg.plug.dj deleter

--- app/bot/events/chat-message.js
+++ app/bot/events/chat-message.js
@@ -36,9 +36,9 @@ module.exports = (bot) => {
             if (data.message.match(/(\[AFK\].*https?:\/\/.*\.(?:png|jpg|gif))/i)) {
                 bot.moderateDeleteChat(data.raw.chatid);
                 bot.sendChat(bot.identifier + "@" + data.user.username + " nitroghost is a sour puss and has banned image/gif AFK responses. pls change it, k thanks bai!");
-            } else if (data.message.match(/.*(https?:\/\/stg.plug.dj\/.*)\s.*/i)) {
+            } else if (data.message.match(/.*(https?:\/\/stg\.plug\.dj).*/i)) {
               bot.moderateDeleteChat(data.raw.chatid);
-              bot.sendChat(bot.identifier + "@" + data.user.username + " please do not post links to plug mmkay?");
+              // bot.sendChat(bot.identifier + "@" + data.user.username + " please do not post links to plug mmkay?");
             } else {
                 bot.db.models.person.findOne({
                     uid: data.user.id
