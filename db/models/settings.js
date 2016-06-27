9b724acbd7e86d26ef00f5c7ad90ef0ee44346d4
Qais Patankar <qaisjp@gmail.com>
2016-06-27 17:00:21 +0100

    clear raffle mongo

--- db/models/settings.js
+++ db/models/settings.js
@@ -1,34 +1,17 @@
 module.exports = (db, mongoose) => {
-    const settingsSchema = new mongoose.Schema({
-        id: {
-            type: String,
-            unique: true
-        },
-        
-        raffle: {
-            enabled: {
-                type: Boolean,
-                default: false
-            },
-            started: {
-                type: {
-                    Boolean,
-                    default: false
-                }
-            },
-            nextRaffleSong: {
-                type: Number,
-                default: -1
-            },
-            users: []
-        },
-        songCount: {
-            type: Number,
-            default: 0
-        }
-    });
-    settingsSchema.index({
-        id: 1
-    });
-    db.model("settings", settingsSchema);
+	const settingsSchema = new mongoose.Schema({
+		id: {
+			type: String,
+			unique: true
+		},
+
+		songCount: {
+			type: Number,
+			default: 0
+		}
+	});
+	settingsSchema.index({
+		id: 1
+	});
+	db.model("settings", settingsSchema);
 };
