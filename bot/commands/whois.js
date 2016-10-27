058e7ce519797e36e4fd06f38ad58e74d677e853
Qais Patankar <qaisjp@gmail.com>
2016-10-27 19:36:15 +0100

    update whois command

--- bot/commands/whois.js
+++ bot/commands/whois.js
@@ -1,26 +1,29 @@
 module.exports = (bot, data) => {
     const user = data.user.username;
-    const rank = data.user.role;
-    //if the user has name in the bot.devs array, or their role is one from bot.rank
-    if (bot.devs.indexOf(user) > -1 || bot.ranks.indexOf(rank) > -1) {
-        if (typeof(data.params) !== "undefined" && data.params.length > 0) {
-            if (data.params.length === 1) {
-                var username = data.params[0];
-                if (username.substr(0, 1) === "@") {
-                    //remove the @
-                    username = username.substr(1);
-                }
-                try {
-                    var person = bot.getUserByName(username);
-                    var kickcount = (typeof(person.kickCount) === "undefined") ? 0 : person.kickCount;
-                    bot.sendChat(bot.identifier + "Username : " + person.username + "\n Played songs : " + person.playedCount + "\n Songs in queue : " + person.songsInQueue + "\n Dubs : " + person.dubs + "\n Kick count : " + kickcount);
-                } catch (e) {
-                    bot.sendChat(bot.identifier + "username is invalid.");
-                }
-            } else {
-                bot.sendChat(bot.identifier + "Please enter a single username.");
-            }
 
+    let username = data.user.username;
+    if (data.params[0] != null) {
+        username = data.params[0].toLowerCase();
+        if (username.substr(0, 1) === "@") {
+            username = username.substr(1);
         }
     }
+
+    bot.rethink
+        .table("users")
+        .getAll(username, { index: "username" })
+        .filter({platform: "dubtrack"})
+        .run().then(docs => {
+            if (docs.length === 0) {
+                bot.sendChat(`@${data.user.username}, I don't know who ${username} is!`);
+                return;
+            } else if (docs.length !== 1) {
+                bot.sendChat(`@${data.user.username}, I found multiple people?? Tell @qaisjp about ${outName}!`);
+                return;
+            }
+
+            const doc = docs[0];
+            bot.sendChat(`${username} - karma(${doc.karma})`);
+            return;
+        });
 };
