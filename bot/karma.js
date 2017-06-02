4143c3604a90245675b32aced74c152c08236c63
Qais Patankar <qaisjp@gmail.com>
2017-06-02 13:41:11 +0100

    pgify karma++

--- bot/karma.js
+++ bot/karma.js
@@ -5,50 +5,44 @@ let bot;
 const event = require('./events/chat-message.js');
 const db = require('./lib/db');
 
-// function onChat(data) {
-// 	const lastChars = data.message.slice(-2);
-// 	if ((lastChars !== "++")) {
-// 		return;
-// 	}
+function onChat(data) {
+	const lastChars = data.message.slice(-2);
+	if ((lastChars !== "++")) {
+		return;
+	}
 
-// 	if (data.message.split(" ").length > 1) {
-// 		return;
-// 	}
+	if (data.message.split(" ").length > 1) {
+		return;
+	}
 	
-// 	let username = data.message.slice(0, -2);
-// 	if (username.substr(0, 1) === "@") {
-// 		username = username.substr(1);
-// 	}
+	let username = data.message.slice(0, -2);
+	if (username.substr(0, 1) === "@") {
+		username = username.substr(1);
+	}
 
-// 	const offsetKarma = (data.user.username.toLowerCase() === username.toLowerCase()) ? -1 : 1;
-// 	r
-// 		.table("users")
-// 		.getAll(username.toLowerCase(), { index: "username_l" })
-// 		.filter({ platform: "dubtrack" })
-// 		.update(
-// 			{ karma: r.row.getField("karma").add(offsetKarma) },
-// 			{ returnChanges: true }
-// 		)
-// 		.run()
-// 		.then(result => {
-// 			if (result.replaced === 0) {
-// 				bot.sendChat(`@${data.user.username}, I don't know who ${username} is!`);
-// 				return;
-// 			} else if (result.replaced !== 1) {
-// 				bot.sendChat(`@${data.user.username}, I found multiple people?? Tell @qaisjp about ${username}!`);
-// 				return;
-// 			}
+	bot.util.getUserIDFromName(username, (nameErr, dubid) => {
+		if (nameErr != null) {
+			bot.sendChat(`@${data.user.username}, Dubtrack doesn't know who ${username} is!`);
+			return;
+		}
 
-// 			const newDoc = result.changes[0].new_val;
-// 			let suffix = "";
+		const whore = data.user.id === dubid;
+		const offsetKarma = whore ? -1 : 1;
 
-// 			if (data.user.username === username) {
-// 				suffix = "No karma whoring for you!";
-// 			}
+		db.query(`UPDATE dubtrack_users SET karma = karma + $2 where dub_id = $1 RETURNING karma`, [dubid, offsetKarma], (err, res) => {
+			if (bot.checkError(err, "pgsql", 'could not update karma++')) {
+				bot.sendChat("Could not update karma++.");
+				return;
+			} else if (res.rowCount === 0) {
+				bot.sendChat(`@${data.user.username}, I don't know who ${username} is!`);
+				return;
+			}
 
-// 			bot.sendChat(`${username} is now at ${newDoc.karma} karma! ${suffix}`);
-// 		});
-// }
+			const suffix = whore ? "No karma whoring for you!" : "";
+			bot.sendChat(`${username} is now at ${res.rows[0].karma} karma! ${suffix}`);
+		});
+	});
+}
 
 
 function commandSet(data) {
@@ -207,6 +201,6 @@ module.exports.init = function init(receivedBot) {
 	bot = receivedBot;
 	
 	event.AddCommand('karma', onCommand);
-//	event.AddHandler("karma", onChat);
+	// event.AddHandler("karma", onChat);
 };
 
