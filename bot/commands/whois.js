4125e753416ad00e876a2762081a7b5a18c3f76d
Qais Patankar <qaisjp@gmail.com>
2017-06-02 16:04:19 +0100

    add whois pg

--- bot/commands/whois.js
+++ bot/commands/whois.js
@@ -1,32 +1,33 @@
+const db = require("../lib/db");
+
 module.exports = (bot, data) => {
-    const user = data.user.username;
+	let username = data.user.username;
+	if (data.params[0] != null) {
+		username = data.params[0];
+		if (username.substr(0, 1) === "@") {
+			username = username.substr(1);
+		}
+	}
 
-    let username = data.user.username;
-    if (data.params[0] != null) {
-        username = data.params[0];
-        if (username.substr(0, 1) === "@") {
-            username = username.substr(1);
-        }
-    }
+	bot.util.getUserIDFromName(username, (nameErr, dubid) => {
+		if (nameErr != null) {
+			bot.sendChat("Could not find user.");
+			return;
+		}
 
-    bot.rethink
-        .table("users")
-        .getAll(username.toLowerCase(), { index: "username_l" })
-        .filter({platform: "dubtrack"})
-        .run().then(docs => {
-            if (docs.length === 0) {
-                bot.sendChat(`@${data.user.username}, I don't know who ${username} is!`);
-                return;
-            } else if (docs.length !== 1) {
-                bot.sendChat(`@${data.user.username}, I found multiple people?? Tell @qaisjp about ${outName}!`);
-                return;
-            }
+		db.query("SELECT * FROM dubtrack_users WHERE dub_id = $1", [dubid],
+			(err, res) => {
+				if (bot.checkError(err, "pgsql", 'could not update karma++')) {
+					bot.sendChat("Could not update karma++.");
+					return;
+				} else if (res.rowCount === 0) {
+					bot.sendChat(`@${data.user.username}, I don't know who ${username} is!`);
+					return;
+				}
 
-            const doc = docs[0];
-            var user = bot.getUserByName(username);
-            user = (user == null) ? {} : user
-
-            bot.sendChat(`${username} | JACR: karma(${doc.karma}), | Dubtrack: songsInQueue(${user.songsInQueue}), dubs(${user.dubs})`);
-            return;
-        });
+				const row = res.rows[0];
+				bot.sendChat(`${username} | JACR: karma(${row.karma}), id(${row.id}) | Dubtrack: id(${row.dub_id})`);
+			}
+		);
+	});
 };
