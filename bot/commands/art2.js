5605d6745a41f1771d709491c47568191ee7442f
Qais Patankar <qaisjp@gmail.com>
2017-11-20 15:06:51 +0000

    Add art2 command

---
+++ bot/commands/art2.js
@@ -0,0 +1,27 @@
+const request = require("request");
+
+module.exports = (bot, data) => {
+	request({
+		url: "https://www.reddit.com/r/artporn/random.json",
+		json: true,
+	}, (error, response, body) => {
+		if (error != null || response.statusCode !== 200) {
+			bot.log("error", "ART2_GET", [response.statusCode, error, body]);
+			return;
+		}
+
+		const rData = body[0].data.children[0].data;
+
+		if (rData.over_18) {
+			module.exports(bot, data);
+			return;
+		}
+
+		let url = rData.url;
+		if (url.slice(-5) === ".gifv") {
+			url = url.slice(0, -1);
+		}
+
+		bot.sendChat(`${data.user.username}: "${rData.title}" ${url}`);
+	});
+};
